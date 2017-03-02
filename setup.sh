40576a32fb2c82cdacef0d9f703ee80441d413c4
syamamori <tnct70941@gmail.com>
2017-03-02 21:32:07 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+cp vimrc ~/.vimrc
+cp -r rc ~/.vim/
+mkdir -p ~/.vim/utils/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git \
+        ~/.vim/dein/repos/github.com/Shougo/dein.vim
