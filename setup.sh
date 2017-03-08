6515ae19f4d19fe526e11376ceb4f7b77bd03235
syamamori <tnct70941@gmail.com>
2017-03-08 18:28:20 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 cp vimrc ~/.vimrc
 cp -r rc ~/.vim/
-if [ ! -e  ~/.vim/utils/dein/repos/github.com/Shougo/ ]; then
-    cd ~/.vim/utils/dein/repos/github.com/Shougo/
-    git clone https://github.com/Shougo/dein.vim.git .
-fi
+
