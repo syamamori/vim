fb8769a53d57fefbb6325f972231481094e66701
syama <tnct70941@gmail.com>
2018-01-27 21:22:35 +0900

    a

--- install_git.sh
+++ install_git.sh
@@ -2,22 +2,19 @@ git clone https://github.com/vim/vim.git ~/vim
 cd ~/vim
 # LDFLAGS="-Wl,-rpath=${HOME}/.pyenv/versions/2.7.13/lib:${HOME}/.pyenv/versions/3.6.2/lib"
 sudo ./configure \
-    --prefix=$HOME/bin \
+    --prefix=$HOME/vim \
     --enable-fail-if-missing \
     --with-features=huge \
-    --enable-clipboard \
     --enable-fontset \
     --enable-multibyte \
     --enable-gui=gtk2 \
     --enable-luainterp \
     --enable-perlinterp \
     --enable-rubyinterp \
-    --enable-tclinterp \
     --enable-pythoninterp=dynamic \
     --enable-python3interp=dynamic \
     --with-luajit \
-    --enable-cscope \
+    --enable-cscope
 
 sudo make
-sudo make reconfig
 sudo make install
